15fe56fbd42611bffe3f358dda7d8045e1551840
infinitypaul <infinitypaul@live.com>
2020-03-15 02:26:56 +0100

    laravel support 2

--- src/Facade/NairaExchangeRatesFacade.php
+++ src/Facade/NairaExchangeRatesFacade.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Facade;
 class NairaExchangeRatesFacade extends Facade
 {
     /**
-     * Get the registered name of the component.
+     * Get the registered name of the component..
      *
      * @return string
      */
